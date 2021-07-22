68fbb4eeaef23a49ea29034d8db49afc485362bb
Kaitlyn Martinez (Intern) <kmartinez1@godaddy.com>
2021-07-22 16:34:35 -0700

    v2

--- pages/upload.js
+++ pages/upload.js
@@ -14,33 +14,45 @@ function Upload(){
 	const [selectedFile, setSelectedFile] = useState();
   
 
-	const changeHandler = async (event) => {
-		var file = event.target.files[0];
-    console.log(event.target.files)
-    var reader = new FileReader();
-    reader.onload = function(event) {
-      console.log(file)
-      setSelectedFile(file.result);
-    };
-	};
-  const formSubmission = async () => {
-    console.log(selectedFile)
-    handleFormSubmission({id:0, text:selectedFile})
-  }
-	const handleFormSubmission = async (resume) => {
-    const body = JSON.stringify(resume)
-    //console.log(body)
+	// const changeHandler = async (event) => {
+	// 	var file = event.target.files[0];
+  //   console.log(event.target.files)
+  //   var reader = new FileReader();
+  //   reader.onload = function(event) {
+  //     console.log(file)
+  //     setSelectedFile(file.result);
+  //   };
+	// };
+  // const formSubmission = async () => {
+  //   console.log(selectedFile)
+  //   handleFormSubmission({id:0, text:selectedFile})
+  // }
+	const handleFormSubmission = async () => {
+    const body = JSON.stringify({text:selectedFile})
+    console.log("body" + body)
     const header = { 'content-type': 'application/json'}
     const response = await fetch(resumeUrl, {method: "POST", body, headers: { 'content-type': 'application/json'}});
     router.push("/upload");
   }
+  let fileReader;
+  const  handleFileReader = async (e) => {
+    const content = fileReader.result;
+    
+    setSelectedFile(content)
+    console.log( "setselectedfile" , selectedFile)
+  };
+  const handleFileChosen = (file) =>{
+      fileReader = new FileReader();
+      fileReader.onloadend = handleFileReader;
+      let text = fileReader.readAsText(file);
+   };
 
   return(
     <Container>
       <NavBar name="Upload"/>
 
-      <input type="file" name="file" onChange={changeHandler} />
-      <button onClick={formSubmission}>Submit</button>
+      <input type="file" name="file" onChange={e=> handleFileChosen(e.target.files[0])} />
+      <button onClick={handleFormSubmission}>Submit</button>
     </Container>
   )
 }
